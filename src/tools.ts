e8b4b73ec86dd67ee25c4b53f2822d0435ad21fc
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-08-29 12:26:10 -0500

    tools updated: management of the new function getCostOfLiving added

--- src/tools.ts
+++ src/tools.ts
@@ -1,20 +1,23 @@
 import { ChatCompletionTool } from 'openai/resources';
 
-import { getRandomCity, getWeather } from './functions';
+import { getCostOfLiving, getRandomCity, getWeather } from './functions';
 
 import tools from '../tools.json';
 
 export const TOOLS = tools as ChatCompletionTool[];
 
-export const getToolOutput = (functionName: string, functionArgs: any) => {
+export const getToolOutput = async (functionName: string, functionArgs: any) => {
   switch (functionName) {
     case 'getWeather':
       return getWeather(functionArgs);
+
+    case 'getCostOfLiving':
+      return getCostOfLiving(functionArgs);
 
     case 'getRandomCity':
       return getRandomCity();
 
     default:
-      return { error: true, message: 'Función desconocida' };
+      return { error: 'unknown function', message: 'function not found' };
   }
 };
